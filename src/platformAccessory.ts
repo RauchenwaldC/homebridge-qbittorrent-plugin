b802ab0e19464011cceab7c71cffb62a1473b3f3
Ben <43026681+bwp91@users.noreply.github.com>
2024-04-20 03:33:15 +0100

    add log success polyfill

--- src/platformAccessory.ts
+++ src/platformAccessory.ts
@@ -58,7 +58,7 @@ export class ExamplePlatformAccessory {
      * this.accessory.getService('NAME') || this.accessory.addService(this.platform.Service.Lightbulb, 'NAME', 'USER_DEFINED_SUBTYPE_ID');
      *
      * The USER_DEFINED_SUBTYPE must be unique to the platform accessory (if you platform exposes multiple accessories, each accessory
-     * can use the same sub type id.)
+     * can use the same subtype id.)
      */
 
     // Example: add two "motion sensor" services to the accessory
@@ -106,7 +106,7 @@ export class ExamplePlatformAccessory {
    * Handle the "GET" requests from HomeKit
    * These are sent when HomeKit wants to know the current state of the accessory, for example, checking if a Light bulb is on.
    *
-   * GET requests should return as fast as possbile. A long delay here will result in
+   * GET requests should return as fast as possible. A long delay here will result in
    * HomeKit being unresponsive and a bad user experience in general.
    *
    * If your device takes time to respond you should update the status of your device
