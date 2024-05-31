646f34f16188fa7938c0fe6746b40da8085c4d35
Ben <43026681+bwp91@users.noreply.github.com>
2024-05-31 18:41:34 +0100

    fix esmodule import paths (extensions)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { API } from 'homebridge';
 
-import { PLATFORM_NAME } from './settings';
-import { ExampleHomebridgePlatform } from './platform';
+import { PLATFORM_NAME } from './settings.js';
+import { ExampleHomebridgePlatform } from './platform.js';
 
 /**
  * This method registers the platform with Homebridge
