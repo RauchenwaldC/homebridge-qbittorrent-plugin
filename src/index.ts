f61a4a6656cc1494aa28f474077c5e7c7d8328ca
Christian Rauchenwald <christian@convertain.com>
2024-09-28 11:50:49 +0200

    Renamed examples into qBittorrent

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import type { API } from 'homebridge';
 
-import { ExampleHomebridgePlatform } from './platform.js';
+import { qBittorrentPlatform } from './platform.js';
 import { PLATFORM_NAME } from './settings.js';
 
 /**
  * This method registers the platform with Homebridge
  */
 export default (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, ExampleHomebridgePlatform);
+  api.registerPlatform(PLATFORM_NAME, qBittorrentPlatform);
 };
